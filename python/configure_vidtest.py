5ac3b2c578033647fe3216235df0a404c317928c
David Helkowski <github@codechild.com>
2021-02-16 17:38:18 -0800

    Add missing script to configure vidapp xcode project

---
+++ python/configure_vidtest.py
@@ -0,0 +1,64 @@
+#!/usr/local/bin/python3
+from pbxproj import XcodeProject
+import json
+
+jsonPath = "muxed.json"
+with open(jsonPath) as f:
+  conf = json.load(f)
+
+project = XcodeProject.load('./repos/vidapp/vidtest/vidtest.xcodeproj/project.pbxproj')
+
+mode = "Release"
+
+def getflag(target,flag):
+  for conf in project.objects.get_configurations_on_targets(target, mode):
+    cdict = conf["buildSettings"]
+    return cdict[flag]
+
+def removeflag(target,flag):
+  val = ""
+  for conf in project.objects.get_configurations_on_targets(target, mode):
+    cdict = conf["buildSettings"]
+    val = cdict[flag]
+  if val is None:
+    return
+  project.remove_flags(flag, val, target, "Debug")
+
+l = "vidtest"
+r = "vidtest2"
+
+vidtest = conf["vidtest"]
+idPrefix = vidtest["bundleIdPrefix"]
+project.set_flags('DEVELOPMENT_TEAM', vidtest["devTeamOu"], r, mode)
+project.set_flags('DEVELOPMENT_TEAM', vidtest["devTeamOu"], l, mode)
+project.set_flags('CODE_SIGN_STYLE', vidtest["main"]["buildStyle"], l, mode)
+project.set_flags('CODE_SIGN_STYLE', vidtest["extension"]["buildStyle"], r, mode)
+project.set_flags('PRODUCT_BUNDLE_IDENTIFIER', idPrefix + ".vidtest2", l, mode)
+project.set_flags('PRODUCT_BUNDLE_IDENTIFIER', idPrefix + ".vidtest2.extension", r, mode)
+
+lProv = vidtest["main"]["provisioningProfile"]
+rProv = vidtest["extension"]["provisioningProfile"]
+
+if lProv == "":
+  removeflag(l, 'PROVISIONING_PROFILE_SPECIFIER')
+else:
+  project.set_flags('PROVISIONING_PROFILE_SPECIFIER', lProv, l, mode)
+
+if rProv == "":
+  removeflag(r, 'PROVISIONING_PROFILE_SPECIFIER')
+else:
+  project.set_flags('PROVISIONING_PROFILE_SPECIFIER', rProv, r, mode)
+
+print("vidtest:")
+print("  Style    : " + ( getflag(l, "CODE_SIGN_STYLE") or "nil" ) )
+print("  Dev Team : " + ( getflag(l, "DEVELOPMENT_TEAM") or "nil" ) )
+print("  Bundle ID: " + getflag(l, "PRODUCT_BUNDLE_IDENTIFIER") )
+print("  Prov Prof: " + ( getflag(l, "PROVISIONING_PROFILE_SPECIFIER") or "nil" ) )
+
+print("vidtest2:")
+print("  Style    : " + ( getflag(r, "CODE_SIGN_STYLE") or "nil" ) )
+print("  Dev Team : " + ( getflag(r, "DEVELOPMENT_TEAM") or "nil" ) )
+print("  Bundle ID: " + getflag(r, "PRODUCT_BUNDLE_IDENTIFIER") )
+print("  Prov Prof: " + ( getflag(r, "PROVISIONING_PROFILE_SPECIFIER") or "nil" ) )
+
+project.save()
